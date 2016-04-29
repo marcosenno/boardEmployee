63350c373eb667cf31819df2ae11e7fc9f05ccb9
Roberto <ballone.calogero.roberto@gmail.com>
2016-04-29 11:42:32 +0200

    base64 tests

---
+++ boardEmployee/GadgeteerApp3/GadgeteerApp3/Class1.cs
@@ -0,0 +1,12 @@
+using System;
+using Microsoft.SPOT;
+using GT = Gadgeteer;
+
+namespace GadgeteerApp3
+{
+    class Class1
+    {
+        public string rfid { get; set; }
+        public byte[] photo { get; set; }
+    }
+}
---
+++ boardEmployee/RFID test/RFID test/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Presentation;
+using Microsoft.SPOT.Presentation.Controls;
+using Microsoft.SPOT.Presentation.Media;
+using Microsoft.SPOT.Presentation.Shapes;
+using Microsoft.SPOT.Touch;
+
+using Gadgeteer.Networking;
+using GT = Gadgeteer;
+using GTM = Gadgeteer.Modules;
+using Gadgeteer.Modules.GHIElectronics;
+
+using Json.NETMF;
+
+namespace RFID_test
+{
+    public partial class Program
+    {
+        private string scannedRFID;
+        private Boolean authInProgress = false;
+        private Boolean networkUp = false;
+        GT.Timer timeOutTimer = new GT.Timer(5000);
+        private string webserverUrl = "localhost";
+
+        Font fontNina = Resources.GetFont(Resources.FontResources.NinaB);
+        // This method is run when the mainboard is powered up or reset.   
+        void ProgramStarted()
+        {
+            Debug.Print("Program Started");
+            ethernetJ11D.UseStaticIP("192.168.1.202", "255.255.255.0", "192.168.1.201");
+            ethernetJ11D.UseThisNetworkInterface();
+            this.rfidReader.IdReceived += this.rfidReader_IdReceived;
+            this.rfidReader.MalformedIdReceived += this.rfidReader_MalformedIdReceived;
+            this.cam.PictureCaptured += camera_PictureCaptured;
+        }
+
+        void camera_PictureCaptured(Camera sender, GT.Picture e)
+        {
+            if (authInProgress)
+            {
+                sendAuthRequest(scannedRFID, e);
+            }
+            else
+            {
+                Debug.Print("Rescan RFID");
+            }
+        }
+
+        private void sendAuthRequest(string scannedRFID, GT.Picture capturedImage)
+        {
+            /* Code for sending rfid and picture to webserver */
+            if (true /*ethernetJ11D.IsNetworkUp*/)
+            {
+                string jsonString = getJsonString(scannedRFID, capturedImage);
+                
+                /*POSTContent jsonContent = POSTContent.CreateTextBasedContent(jsonString);
+                var req = HttpHelper.CreateHttpPostRequest(webserverUrl, jsonContent, "application/json");
+                req.ResponseReceived += new HttpRequest.ResponseHandler(req_ResponseReceived);
+                req.SendRequest();
+                 */
+            }
+            else
+            {
+                Debug.Print("Authentication failed because network is down");
+            }
+
+            authInProgress = false;
+            timeOutTimer.Stop();
+        }
+
+        void req_ResponseReceived(HttpRequest sender, HttpResponse response)
+        {
+            if (response.StatusCode != "200")
+            {
+                Debug.Print(response.StatusCode);
+                networkUp = false;
+            }
+
+            Debug.Print("Response received");
+        }
+
+        string getJsonString(string scannedRFID, GT.Picture capturedImage)
+        {
+            //string jsonString = "";
+            CRequestData testObj = new CRequestData();
+            testObj.RFID = scannedRFID;
+            testObj.image = capturedImage;
+            string json = JsonSerializer.SerializeObject(testObj);
+            Debug.Print(json);
+            return json;
+        }
+
+        private GT.Timer cameraRetryTimer = new GT.Timer(100);
+        int cameraRetryCount;
+
+        private void rfidReader_IdReceived(RFIDReader sender, string e)
+        {
+            //cameraRetryTimer.Tick += cameraRetryTimer_Tick;
+            timeOutTimer.Tick += timeOutTimer_Tick;
+            if (authInProgress == false)
+            {
+                Debug.Print("RFID scanned: " + e);
+                scannedRFID = e;
+                if (cam.CameraReady)
+                {
+                    authInProgress = true;
+                    cam.TakePicture();
+                    timeOutTimer.Start();
+                }
+                else
+                {
+                    Debug.Print("Camera not ready");   
+                     /* cameraRetryTimer.Start();
+                    cameraRetryCount = 0;
+                     */
+                }
+            }
+            else
+            {
+                Debug.Print("Authentication already in progress");
+            }
+            /*displayTE35.SimpleGraphics.Clear();
+            displayTE35.SimpleGraphics.DisplayText("RFID scanned: " + e, fontNina, GT.Color.White, 10, 10);*/
+        }
+        /*
+        void cameraRetryTimer_Tick(GT.Timer timer)
+        {
+            if (cam.CameraReady)
+            {
+                authInProgress = true;
+                cam.TakePicture();
+                timeOutTimer.Start();
+                cameraRetryTimer.Stop();
+            }
+            else
+            {
+                if (cameraRetryCount > 10)
+                {
+                    Debug.Print("Not able to take picture");
+                    cameraRetryTimer.Stop();
+                }
+                cameraRetryCount += 1;
+                Debug.Print("Camera retry count: " + cameraRetryCount.ToString());
+            }
+        }*/
+
+        void timeOutTimer_Tick(GT.Timer timer)
+        {
+            if (authInProgress)
+            {
+                authInProgress = false;
+                Debug.Print("Authentication failed because of time-out");
+            }
+            timeOutTimer.Stop();
+        }
+
+        private void rfidReader_MalformedIdReceived(RFIDReader sender, EventArgs e)
+        {
+            Debug.Print("Please rescan your card");
+            displayTE35.SimpleGraphics.Clear();
+            displayTE35.SimpleGraphics.DisplayText("Please rescan your card", fontNina, GT.Color.White, 10, 10);
+        }
+    }
+}
